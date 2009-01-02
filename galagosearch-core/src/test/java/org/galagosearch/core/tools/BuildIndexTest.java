98a2b3bbd98bf8c23ac8dbfa0dc49b53a640e676
trevor.strohman <trevor.strohman@029f083a-8788-11dd-9299-37fd5bf03096>
2009-01-02 19:54:51 +0000

    Refactors most of App into BuildIndex.  Also adds usage information and a little bit of error handling.

---
+++ galagosearch-core/src/test/java/org/galagosearch/core/tools/BuildIndexTest.java
@@ -0,0 +1,145 @@
+// BSD License (http://www.galagosearch.org/license)
+
+package org.galagosearch.core.tools;
+
+import junit.framework.TestCase;
+import org.galagosearch.tupleflow.execution.ErrorStore;
+import org.galagosearch.tupleflow.execution.Job;
+import org.galagosearch.tupleflow.execution.Verification;
+
+/**
+ *
+ * @author trevor
+ */
+public class BuildIndexTest extends TestCase {
+    
+    public BuildIndexTest(String testName) {
+        super(testName);
+    }
+
+        /**
+     * Test of getSplitStage method, of class BuildIndex.
+     */
+    public void testGetSplitStage() {
+        BuildIndex BuildIndex = new BuildIndex();
+        Job job = new Job();
+        job.add(BuildIndex.getSplitStage("fake-filename"));
+        ErrorStore store = new ErrorStore();
+
+        Verification.verify(job, store);
+        assertEquals(0, store.getErrors().size());
+        assertEquals(0, store.getWarnings().size());
+    }
+
+    /**
+     * Test of getParseStage method, of class BuildIndex.
+     */
+    public void testGetParseStage() {
+        BuildIndex BuildIndex = new BuildIndex();
+        Job job = new Job();
+        job.add(BuildIndex.getParsePostingsStage());
+        ErrorStore store = new ErrorStore();
+
+        Verification.verify(job, store);
+        assertEquals(0, store.getErrors().size());
+        assertEquals(0, store.getWarnings().size());
+    }
+
+    /**
+     * Test of getLinkCombineStage method, of class BuildIndex.
+     */
+    public void testGetLinkCombineStage() {
+        BuildIndex BuildIndex = new BuildIndex();
+        Job job = new Job();
+        job.add(BuildIndex.getLinkCombineStage());
+        ErrorStore store = new ErrorStore();
+
+        Verification.verify(job, store);
+        assertEquals(0, store.getErrors().size());
+        assertEquals(0, store.getWarnings().size());
+    }
+
+    /**
+     * Test of getWritePostingsStage method, of class BuildIndex.
+     */
+    public void testGetWritePostingsStage() {
+        BuildIndex BuildIndex = new BuildIndex();
+        Job job = new Job();
+        job.add(BuildIndex.getWritePostingsStage());
+        ErrorStore store = new ErrorStore();
+
+        Verification.verify(job, store);
+        assertEquals(0, store.getErrors().size());
+        assertEquals(0, store.getWarnings().size());
+    }
+
+    /**
+     * Test of getWriteExtentsStage method, of class BuildIndex.
+     */
+    public void testGetWriteExtentsStage() {
+        BuildIndex BuildIndex = new BuildIndex();
+        Job job = new Job();
+        job.add(BuildIndex.getWriteExtentsStage());
+        ErrorStore store = new ErrorStore();
+
+        Verification.verify(job, store);
+        assertEquals(0, store.getErrors().size());
+        assertEquals(0, store.getWarnings().size());
+    }
+
+    /**
+     * Test of getWriteDatesStage method, of class BuildIndex.
+     */
+    public void testGetWriteDatesStage() {
+        BuildIndex BuildIndex = new BuildIndex();
+        Job job = new Job();
+        job.add(BuildIndex.getWriteDatesStage());
+        ErrorStore store = new ErrorStore();
+
+        Verification.verify(job, store);
+        assertEquals(0, store.getErrors().size());
+        assertEquals(0, store.getWarnings().size());
+    }
+
+    /**
+     * Test of getWriteManifestStage method, of class BuildIndex.
+     */
+    public void testGetWriteManifestStage() {
+        BuildIndex BuildIndex = new BuildIndex();
+        Job job = new Job();
+        job.add(BuildIndex.getWriteManifestStage());
+        ErrorStore store = new ErrorStore();
+
+        Verification.verify(job, store);
+        assertEquals(0, store.getErrors().size());
+        assertEquals(0, store.getWarnings().size());
+    }
+
+    /**
+     * Test of getWriteDocumentLengthsStage method, of class BuildIndex.
+     */
+    public void testGetWriteDocumentLengthsStage() {
+        BuildIndex BuildIndex = new BuildIndex();
+        Job job = new Job();
+        job.add(BuildIndex.getWriteDocumentLengthsStage());
+        ErrorStore store = new ErrorStore();
+
+        Verification.verify(job, store);
+        assertEquals(0, store.getErrors().size());
+        assertEquals(0, store.getWarnings().size());
+    }
+
+    /**
+     * Test of getIndexJob method, of class BuildIndex.
+     */
+    public void testGetIndexJob() {
+        BuildIndex BuildIndex = new BuildIndex();
+        Job job = BuildIndex.getIndexJob("one", "two");
+        ErrorStore store = new ErrorStore();
+
+        Verification.verify(job, store);
+        assertEquals("", store.toString());
+        assertEquals(0, store.getErrors().size());
+        assertEquals(0, store.getWarnings().size());
+    }
+}
