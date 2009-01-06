a73e045c426767d72e71821b3a6202768e8aa1cd
trevor.strohman <trevor.strohman@029f083a-8788-11dd-9299-37fd5bf03096>
2009-01-06 05:29:05 +0000

    Added better ExtentInsideIterator comments.

--- trunk/galagosearch-core/src/main/java/org/galagosearch/core/retrieval/structured/ExtentInsideIterator.java
+++ trunk/galagosearch-core/src/main/java/org/galagosearch/core/retrieval/structured/ExtentInsideIterator.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import org.galagosearch.tupleflow.Parameters;
 
 /**
+ * <p>Implements the #inside operator.  The #inside operator is usually implicit
+ * in the query language, where <tt>a.b</tt> is equivalent to <tt>#inside(a b)</tt>.
+ * This is usually used to find terms that occur in fields.  For example,
+ * <tt>#1(bruce croft).author</tt>, which finds instances of "bruce croft" occurring
+ * in the author field of a document.</p>
  *
  * @author trevor
  */
@@ -12,7 +17,21 @@ public class ExtentInsideIterator extends ExtentConjunctionIterator {
     ExtentIterator innerIterator;
     ExtentIterator outerIterator;
 
-    /** Creates a new instance of ExtentInsideIterator */
+    /**
+     * <p>Constructs an #inside instance.  For <tt>#inside(a b)</tt>, this
+     * produces an extent whenever <tt>a</tt> is found inside <tt>b</tt>.</p>
+     *
+     * <p>For example, in the expression <tt>#inside(#1(white house) #extents:title())</tt>,
+     * <tt>#1(white house)</tt> is the inner iterator and <tt>#extents:title()</tt>
+     * is the outer iterator.  Whenever <tt>#1(white house)</tt> is found in the title of
+     * a document, this is a match.  The extent for <tt>#1(white house)</tt> is returned
+     * (not the extent for <tt>#extents:title()</tt> that surrounds it).</tt>
+     *
+     * @param parameters extra parameters, not used for anything.
+     * @param innerIterator The source of extents that must be inside.
+     * @param outerIterator The source of extents that must contain the inner extents.
+     * @throws java.io.IOException
+     */
     public ExtentInsideIterator(Parameters parameters,
             ExtentIterator innerIterator,
             ExtentIterator outerIterator) throws IOException {
@@ -21,6 +40,12 @@ public class ExtentInsideIterator extends ExtentConjunctionIterator {
         this.outerIterator = outerIterator;
         findDocument();
     }
+
+    /**
+     * This method is called whenever the ExtentConjunctionIterator has verified
+     * that both the inner and outer iterators match this document.  This method's job
+     * is to find all matchin extents within the document, if they exist.
+     */
 
     public void loadExtents() {
         ExtentArrayIterator inner = new ExtentArrayIterator(innerIterator.extents());
