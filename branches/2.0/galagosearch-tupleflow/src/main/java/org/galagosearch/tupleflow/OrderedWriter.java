0dd404f921accab8f567b5eb5b0af64a1992a784
sjh900 <sjh900@029f083a-8788-11dd-9299-37fd5bf03096>
2012-03-04 16:41:52 +0000

    removed branch

--- branches/2.0/galagosearch-tupleflow/src/main/java/org/galagosearch/tupleflow/OrderedWriter.java
+++
@@ -1,9 +0,0 @@
-// BSD License (http://galagosearch.org/license)
-
-package org.galagosearch.tupleflow;
-
-import java.io.IOException;
-
-public abstract class OrderedWriter<T> implements Processor<T> {
-    public abstract void process(T object) throws IOException;
-}
